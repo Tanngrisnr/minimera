0aeb7f2db7759d354efab14ad133a1667e00698d
Tanngrisnr <fredrik.wintzell@gmail.com>
2021-10-19 16:13:46 +0200

    :lipstick: started theming

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { AuthProvider } from "../contexts/AuthContext";
 import { DataProvider } from "../contexts/DataContext";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Container } from "@mui/material";
+import { createTheme, ThemeProvider, styled } from "@mui/material/styles";
 import Signup from "./Signup";
 import Login from "./Login";
 import Profile from "./Profile";
@@ -14,27 +15,50 @@ import CreateAd from "./CreateAd";
 import Navigation from "./Navigation";
 import About from "./About";
 
+export const themeConfig = createTheme({
+  palette: {
+    type: "light",
+    primary: {
+      main: "#0A8B74",
+      light: "#EBF4F3",
+    },
+    secondary: {
+      main: "#f50057",
+    },
+  },
+  typography: {
+    h1: {
+      fontFamily: "Belleza",
+    },
+  },
+});
+
 function App() {
   return (
-    <Container>
-      <Router>
-        <AuthProvider>
-          <DataProvider>
-            <Switch>
-              <PrivateRoute exact path="/" component={Dashboard} />
-              <PrivateRoute path="about" component={About} />
-              <PrivateRoute path="/create-ad" component={CreateAd} />
-              <PrivateRoute path="/profile" component={Profile} />
-              <PrivateRoute path="/update-profile" component={UpdateProfile} />
-              <Route path="/signup" component={Signup} />
-              <Route path="/login" component={Login} />
-              <Route path="/forgot-password" component={ForgotPassword} />
-            </Switch>
-            <Navigation />
-          </DataProvider>
-        </AuthProvider>
-      </Router>
-    </Container>
+    <ThemeProvider theme={themeConfig}>
+      <Container>
+        <Router>
+          <AuthProvider>
+            <DataProvider>
+              <Switch>
+                <PrivateRoute exact path="/" component={Dashboard} />
+                <PrivateRoute path="about" component={About} />
+                <PrivateRoute path="/create-ad" component={CreateAd} />
+                <PrivateRoute path="/profile" component={Profile} />
+                <PrivateRoute
+                  path="/update-profile"
+                  component={UpdateProfile}
+                />
+                <Route path="/signup" component={Signup} />
+                <Route path="/login" component={Login} />
+                <Route path="/forgot-password" component={ForgotPassword} />
+              </Switch>
+              <Navigation />
+            </DataProvider>
+          </AuthProvider>
+        </Router>
+      </Container>
+    </ThemeProvider>
   );
 }
 
